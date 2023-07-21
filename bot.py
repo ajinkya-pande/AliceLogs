50670b6cf1d87a37394473ae59b4ed5057e792b8
WaterMelo <74883609+WaterMeloDev@users.noreply.github.com>
2023-07-21 12:30:46 -0500

    Update bot.py with more info

--- bot.py
+++ bot.py
@@ -56,6 +56,9 @@ async def change_status():
 async def help_cmd(interaction: discord.Interaction):
     embed = discord.Embed(title="Help", color=0xFFFFFF)
     embed.add_field(name="Infomation", value="I monitor your server on Twitch ;>.", inline=False)
+    embed.add_field(name="Support", value="https://discord.gg/6dCMQt33aw", inline=False)
+    embed.add_field(name="Vote for me!", value="[Vote](https://universe-list.xyz/bots/1131451736247255091)", inline=False)
+    embed.add_field(name="Invite me!", value="[Invite](https://discord.com/api/oauth2/authorize?client_id=1131451736247255091&permissions=2611341028544&scope=bot)", inline=False)
     await interaction.response.send_message(embed=embed)
 
 async def main():
