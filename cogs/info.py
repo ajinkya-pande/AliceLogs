f34c571cb2986a8bf808ca47b11d74db94a854fe
watermelodev <3up.jon@gmail.com>
2023-07-21 12:21:18 -0500

    updated help commands

--- cogs/info.py
+++ cogs/info.py
@@ -13,6 +13,9 @@ class Info(commands.Cog):
     async def help_cmd(self, ctx):
         embed = discord.Embed(title="Help", color=0xFFFFFF)
         embed.add_field(name="Infomation", value="I monitor your server on Twitch ;>", inline=False)
+        embed.add_field(name="Support", value="https://discord.gg/6dCMQt33aw", inline=False)
+        embed.add_field(name="Vote for me!", value="[Vote](https://universe-list.xyz/bots/1131451736247255091)", inline=False)
+        embed.add_field(name="Invite me!", value="[Invite](https://discord.com/api/oauth2/authorize?client_id=1131451736247255091&permissions=2611341028544&scope=bot)", inline=False)
         await ctx.send(embed=embed)
 
 async def setup(bot):
